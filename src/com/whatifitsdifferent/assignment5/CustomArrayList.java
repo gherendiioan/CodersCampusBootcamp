39263228af98c16ec26f36ada9807d1b09cf3f3a
John Gherendi <gherendi.ioan@gmail.com>
2023-07-15 21:14:46 +0100

    The original solution for the Assignment5 for Coders Campus. Now reorganizing the GH repo

---
+++ src/com/whatifitsdifferent/assignment5/CustomArrayList.java
@@ -0,0 +1,54 @@
+package com.whatifitsdifferent.assignment5;
+
+public class CustomArrayList<T> implements CustomList<T> {
+	
+	Object[] items = new Object[10];
+	int variableSizeToBeIncreased = 0;
+
+	@Override
+	public boolean add(T item) {
+
+		if (variableSizeToBeIncreased == items.length) {
+			
+			items = extendArraySize();
+			
+		}
+		
+		items[variableSizeToBeIncreased] = item;
+		variableSizeToBeIncreased++;
+		
+		return true;
+		
+	}
+
+	private Object[] extendArraySize() {
+
+		Object[] newArray = new Object[variableSizeToBeIncreased * 2];
+		
+		for (int i = 0; i < variableSizeToBeIncreased; i++) {
+			
+			newArray[i] = items[i];
+			
+		}
+		
+		return newArray;
+		
+	}
+
+	@Override
+	public int getSize() {
+		
+		return variableSizeToBeIncreased;
+		
+	}
+
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public T get(int index) {
+		
+		return (T) items[index];
+		
+	}
+
+}
---
+++ src/com/whatifitsdifferent/assignment5/CustomArrayListApp.java
@@ -0,0 +1,22 @@
+package com.whatifitsdifferent.assignment5;
+
+public class CustomArrayListApp {
+
+	public static void main(String[] args) {
+		CustomList<Integer> numbers = new CustomArrayList<>();
+		
+		for (int i=1; i<=121; i++) {
+			
+			numbers.add(i);
+			
+		}
+		
+		for (int i=0; i<numbers.getSize(); i++) {
+			
+			System.out.println(numbers.get(i));
+			
+		}
+
+	}
+
+}
