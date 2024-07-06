0725be98029152daaa47ad77538e6bf271c45a37
John Gherendi <gherendi.ioan@gmail.com>
2024-07-06 11:17:32 +0100

    Format Student class

--- Assignment4-RecapVersion/src/com/coderscampus/assignment4/recapversion/Student.java
+++ Assignment4-RecapVersion/src/com/coderscampus/assignment4/recapversion/Student.java
@@ -1,27 +1,23 @@
 package com.coderscampus.assignment4.recapversion;
 
 public abstract class Student {
-	
+
 	protected int ID;
 	protected String name;
 	protected String course;
 	protected int grade;
-	
+
 	public Student() {
-		
+
 	}
-	
-	
+
 	public Student(String course) {
 		this.course = course;
 	}
-
 
 	@Override
 	public String toString() {
 		return "Student [ID=" + ID + ", name=" + name + ", course=" + course + ", grade=" + grade + "]";
 	}
-	
-	
-	
+
 }
