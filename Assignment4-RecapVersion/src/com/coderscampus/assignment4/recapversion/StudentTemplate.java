7c4177289b4e5bb698bfbfd955bba08b59d78aad
John Gherendi <gherendi.ioan@gmail.com>
2024-07-12 07:30:14 +0100

    Rename Student -> StudentTemplate
    Remove ID and grade fields. Remove constructors.
    Generate getters and setters for remaining fields (name and course);

---
+++ Assignment4-RecapVersion/src/com/coderscampus/assignment4/recapversion/StudentTemplate.java
@@ -0,0 +1,23 @@
+package com.coderscampus.assignment4.recapversion;
+
+public abstract class StudentTemplate {
+
+	protected String name;
+	protected String course;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	
+	
+
+}
