d89e88f08242387ea9b5ec5eb7210ef86d0cd975
John Gherendi <gherendi.ioan@gmail.com>
2024-07-12 20:05:01 +0100

    Add StudentFactory interface

---
+++ Assignment4-RecapVersion/src/com/coderscampus/assignment4/recapversion/StudentFactory.java
@@ -0,0 +1,11 @@
+package com.coderscampus.assignment4.recapversion;
+
+public interface StudentFactory {
+
+	COMPSCI createCOMPSCIStudent();
+
+	APMTH createAPMTHStudent();
+
+	STAT createSTATStudent();
+
+}
