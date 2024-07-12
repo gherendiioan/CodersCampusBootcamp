6a7d43626c06c0446990e8a20a991931721c7367
John Gherendi <gherendi.ioan@gmail.com>
2024-07-12 19:54:01 +0100

    Make APMTH abstract,extends StudentTemplate
    Remove name field. Remove constructors.
    Generate getters and setters for ID and grade fields.

--- Assignment4-RecapVersion/src/com/coderscampus/assignment4/recapversion/APMTH.java
+++ Assignment4-RecapVersion/src/com/coderscampus/assignment4/recapversion/APMTH.java
@@ -1,21 +1,24 @@
 package com.coderscampus.assignment4.recapversion;
 
-public class APMTH extends Student {
+public abstract class APMTH extends StudentTemplate {
 
 	private int ID;
-	private String name;
 	private int grade;
 
-	public APMTH(String course, int iD, String name, int grade) {
-		super(course);
-		ID = iD;
-		this.name = name;
-		this.grade = grade;
+	public int getID() {
+		return ID;
 	}
 
-	@Override
-	public String toString() {
-		return "APMTH [ID=" + ID + ", name=" + name + ", grade=" + grade + ", course=" + course + "]";
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
 	}
 
 }
