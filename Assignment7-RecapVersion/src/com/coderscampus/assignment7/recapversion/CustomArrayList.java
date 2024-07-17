bb7e12162d76a5bdada905bab9cb9694159da593
John Gherendi <gherendi.ioan@gmail.com>
2024-07-17 06:55:48 +0100

    Add T get() and remove().

--- Assignment7-RecapVersion/src/com/coderscampus/assignment7/recapversion/CustomArrayList.java
+++ Assignment7-RecapVersion/src/com/coderscampus/assignment7/recapversion/CustomArrayList.java
@@ -1,15 +1,13 @@
 package com.coderscampus.assignment7.recapversion;
 
 public class CustomArrayList<T> implements CustomList<T> {
+	
 	Object[] items = new Object[10];
 	int arrayIncrement = 0;
 
+	
 	@Override
 	public boolean add(T item) {
-//		items = (arrayIncrement == items.length) ? increaseArray(): items;
-//		
-//		items[arrayIncrement] = item;
-//		arrayIncrement++;
 
 		return add(arrayIncrement, item);
 	}
@@ -24,42 +22,54 @@ public class CustomArrayList<T> implements CustomList<T> {
 		return increasedArray;
 	}
 
+	
 	@Override
 	public boolean add(int index, T item) throws IndexOutOfBoundsException {
 		if (index > arrayIncrement || index < 0) {
 			throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
 		}
-		
+
 		items = (arrayIncrement == items.length) ? increaseArray() : items;
-		
-		
+
 		System.arraycopy(items, index, items, index + 1, arrayIncrement - index);
-//		for (int i = arrayIncrement - 1; i >= index; i--) {
-////			items[i+1] = (arrayIncrement == items.length) ? increaseArray(): items[i];
-//			items[i + 1] = items[i];
-//		}
 
 		items[index] = item;
 		arrayIncrement++;
 		return true;
 	}
 
+	
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return arrayIncrement;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public T get(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		if (index > arrayIncrement) {
+			throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
+		}
+		return (T) items[index];
 	}
 
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	public T remove(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		if (index > arrayIncrement) {
+			throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
+		}
+
+		T removedItem = (T) items[index];
+
+		for (int i = index; i < arrayIncrement - 1; i++) {
+			items[i] = items[i + 1];
+		}
+		
+		arrayIncrement--;
+		return removedItem;
 	}
 
 }
