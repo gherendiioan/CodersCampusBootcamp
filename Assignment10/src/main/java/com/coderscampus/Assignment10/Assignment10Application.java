38ba2d56a02e4d4a8e3a15b65e8a785553b30ae6
John Gherendi <gherendi.ioan@gmail.com>
2024-08-01 06:30:34 +0100

    Add default Maven project

---
+++ Assignment10/src/main/java/com/coderscampus/Assignment10/Assignment10Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Assignment10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment10Application.class, args);
+	}
+
+}
---
+++ Assignment10/src/test/java/com/coderscampus/Assignment10/Assignment10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Assignment10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
