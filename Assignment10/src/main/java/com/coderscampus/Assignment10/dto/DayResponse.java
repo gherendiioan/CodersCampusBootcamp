7c451f8c41b9c8595d1a7e7fcc834e8211a500e5
John Gherendi <gherendi.ioan@gmail.com>
2024-08-03 22:21:41 +0100

    Create DayResponse DTO

---
+++ Assignment10/src/main/java/com/coderscampus/Assignment10/dto/DayResponse.java
@@ -0,0 +1,31 @@
+package com.coderscampus.Assignment10.dto;
+
+import java.util.List;
+
+public class DayResponse {
+
+	private List<Meal> meals;
+	private Nutrients nutrients;
+
+	public List<Meal> getMeals() {
+		return meals;
+	}
+
+	public void setMeals(List<Meal> meals) {
+		this.meals = meals;
+	}
+
+	public Nutrients getNutrients() {
+		return nutrients;
+	}
+
+	public void setNutrients(Nutrients nutrients) {
+		this.nutrients = nutrients;
+	}
+
+	@Override
+	public String toString() {
+		return "DayResponse [meals=" + meals + ", nutrients=" + nutrients + "]";
+	}
+
+}
