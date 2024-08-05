652b869f90521f0cf790bbd8c2f78a24ab569630
John Gherendi <gherendi.ioan@gmail.com>
2024-08-05 18:30:41 +0100

    Update getWeekMeals .queryParam(timeframe, week)

--- Assignment10/src/main/java/com/coderscampus/Assignment10/service/MealPlanService.java
+++ Assignment10/src/main/java/com/coderscampus/Assignment10/service/MealPlanService.java
@@ -12,70 +12,60 @@ import com.coderscampus.Assignment10.dto.WeekResponseDTO;
 
 @Service
 public class MealPlanService {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(MealPlanService.class);
-	
+
 	@Value("${spoonacular.api.key}")
 	private String apiKey;
-	
+
 	@Value("${spoonacular.urls.base}")
 	private String baseUrl;
-	
-	
+
 	@Value("${spoonacular.urls.mealplan}")
 	private String mealPlanEndpoint;
-	
+
 	private final RestTemplate restTemplate;
-	
+
 	public MealPlanService() {
 		this.restTemplate = new RestTemplate();
-		
+
 	}
-	
+
 	public DayResponseDTO getDayMeals(Integer numCalories, String diet, String exclusions) {
-		
-		String url = UriComponentsBuilder.fromHttpUrl(baseUrl + mealPlanEndpoint)
-				.queryParam("timeFrame", "day")
-				.queryParam("targetCalories", numCalories)
-				.queryParam("diet", diet)
-				.queryParam("exclude", exclusions)
-				.queryParam("apiKey", apiKey)
-				.toUriString();
-		
-		logger.info("fetching daily meal plan: calories = {}, diet = {}, exclusions = {}",
-																numCalories, diet, exclusions);
-		
+
+		String url = UriComponentsBuilder.fromHttpUrl(baseUrl + mealPlanEndpoint).queryParam("timeFrame", "day")
+				.queryParam("targetCalories", numCalories).queryParam("diet", diet).queryParam("exclude", exclusions)
+				.queryParam("apiKey", apiKey).toUriString();
+
+		logger.info("fetching daily meal plan: calories = {}, diet = {}, exclusions = {}", numCalories, diet,
+				exclusions);
+
 		try {
-			
+
 			DayResponseDTO response = restTemplate.getForObject(url, DayResponseDTO.class);
 			logger.debug("Received daily meal plan: {}", response);
 			return response;
-		}catch (Exception e) {
+		} catch (Exception e) {
 			logger.error("Error fetching daily meal plan", e);
 			throw e;
 		}
-		
+
 	}
-	
+
 	public WeekResponseDTO getWeekMeals(Integer numCalories, String diet, String exclusions) {
-		String url = UriComponentsBuilder.fromHttpUrl(baseUrl + mealPlanEndpoint)
-				.queryParam("timeFrame", "day")
-				.queryParam("targetCalories", numCalories)
-				.queryParam("diet", diet)
-				.queryParam("exclude", exclusions)
-				.queryParam("apiKey", apiKey)
-				.toUriString();
 		
+		String url = UriComponentsBuilder.fromHttpUrl(baseUrl + mealPlanEndpoint).queryParam("timeFrame", "week")
+				.queryParam("targetCalories", numCalories).queryParam("diet", diet).queryParam("exclude", exclusions)
+				.queryParam("apiKey", apiKey).toUriString();
+
 		try {
 			WeekResponseDTO response = restTemplate.getForObject(url, WeekResponseDTO.class);
 			logger.debug("Received weekly meal plan: {}", response);
 			return response;
-		}catch (Exception e) {
+		} catch (Exception e) {
 			logger.error("Error fetching weekly meal plan", e);
 			throw e;
 		}
 	}
-	
-	
 
 }
