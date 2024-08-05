1d10b3823b61dede0af39d005fc33013a2e5739c
John Gherendi <gherendi.ioan@gmail.com>
2024-08-05 18:48:53 +0100

    Add properties, pom and main class

--- Assignment10/src/main/java/com/coderscampus/Assignment10/Assignment10Application.java
+++ Assignment10/src/main/java/com/coderscampus/Assignment10/Assignment10Application.java
@@ -1,13 +1,23 @@
 package com.coderscampus.Assignment10;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Assignment10Application {
 
+	private static final Logger logger = LoggerFactory.getLogger(Assignment10Application.class);
+
 	public static void main(String[] args) {
+		
+		logger.info("Starting meal planner application...");
+		
 		SpringApplication.run(Assignment10Application.class, args);
+		
+		logger.info("Meal planner application started succesfully");
+		
 	}
 
 }
